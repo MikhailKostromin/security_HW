694d1e4da62fd2111d5d221b5e8bb468b38b8228
mihailkostromin <kostrominmi@mail.ru>
2024-02-12 23:52:10 +0300

    Initial commit

---
+++ src/main/java/com/example/security_HW/SecurityHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/security_HW/SecurityHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
